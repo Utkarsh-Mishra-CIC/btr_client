10872aa3b850e768fa477e985aae7d9bcc40b7d2
Dashamir Hoxha <dashohoxha@gmail.com>
2017-08-24 23:16:54 +0200

    Fix cfg/set-oauth2-login

--- ds/cfg/set-oauth2-login.sh
+++ ds/cfg/set-oauth2-login.sh
@@ -2,8 +2,12 @@
 
 source /host/settings.sh
 
-alias=${1:-@local_bcl}
+alias=${1:-@bcl}
+server_url=${2:-$OAUTH2_SERVER_URL}
+client_id=${3:-$OAUTH2_CLIENT_ID}
+client_secret=${4:-$OAUTH2_CLIENT_SECRET}
 skip_ssl=1
+
 drush --yes $alias php-script $CODE_DIR/ds/cfg/set-oauth2-login.php  \
-    "$OAUTH2_SERVER_URL" "$OAUTH2_CLIENT_ID" "$OAUTH2_CLIENT_SECRET" "$skip_ssl"
+    "$server_url" "$client_id" "$client_secret" "$skip_ssl"
 drush --yes $alias cc all
