1511cb89de26e7aeeba8a8d8286bf81f51f2de0b
Dashamir Hoxha <dashohoxha@gmail.com>
2017-08-24 23:15:54 +0200

    fix twitter cron

--- ds/cfg/install/twitter.sh
+++ ds/cfg/install/twitter.sh
@@ -5,8 +5,8 @@ source /host/settings.sh
 # setup cron
 cat <<EOF > /etc/cron.d/twitter
 ### first create a user with `adduser twitter`
-0 4 * * *  twitter  /var/www/bcl/profiles/btr_client/utils/twitter.sh > /dev/null 2>&1
+0 4 * * *  twitter  $DRUPAL_DIR/profiles/btr_client/utils/twitter.sh > /dev/null 2>&1
 
 ### uncomment this line only for debugging
-#*/5 * * * *  twitter  /var/www/bcl/profiles/btr_client/utils/twitter.sh
+#*/5 * * * *  twitter  $DRUPAL_DIR/profiles/btr_client/utils/twitter.sh
 EOF
